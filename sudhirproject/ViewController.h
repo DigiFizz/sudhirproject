ccef8ca77c0f69a14c07f269eaa6eefc6bb7a69a
DigiFizz <gautam@GAUTAMs-Mac-mini.local>
2014-07-22 17:24:01 +0530

    Initial Commit

---
+++ sudhirproject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sudhirproject
+//
+//  Created by digifizz on 22/07/14.
+//  Copyright (c) 2014 DIZIFIZZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ sudhirprojectTests/sudhirprojectTests.m
@@ -0,0 +1,34 @@
+//
+//  sudhirprojectTests.m
+//  sudhirprojectTests
+//
+//  Created by digifizz on 22/07/14.
+//  Copyright (c) 2014 DIZIFIZZ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface sudhirprojectTests : XCTestCase
+
+@end
+
+@implementation sudhirprojectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
